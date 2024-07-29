fb6131ddf86d52e1e4b99aff4902c2da3abe5c4e
Jumba <janthegreatxc@gmail.com>
2024-07-29 20:01:06 +0800

    C# Strings

--- Track 1/C# Strings/Program.cs
+++ Track 1/C# Strings/Program.cs
@@ -27,6 +27,49 @@ namespace PrideAndPrejudice
 
       // Print results
       Console.WriteLine($"The user password is {password}. Its length is {passwordLength} and it receives a {passwordCheck} check.");
+
+      // dna strand
+      string startStrand = "ATGCGATGAGCTTAC";
+
+      // find location of "tga"
+      int tga = startStrand.IndexOf("TGA");
+
+      // start point and stop point variables
+      int startPoint = 0;
+      int length = tga + 3;
+
+      // define final strand
+      string dna = startStrand.Substring(startPoint,length);
+      Console.WriteLine(dna);
+      // DNA mutation search
+      Console.WriteLine(dna[3]);
+      if (dna[3] == 'G') {
+        Console.WriteLine("No Mutation");
+      } else {
+        Console.WriteLine("No Mutation");
+      }
+
+      // Script line
+      string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+      // Get camera directions
+      int charPosition = script.IndexOf("Close");
+      int length = "Close on".Length;
+      string cameraDirections = script.Substring(charPosition, length);
+
+      // Get scene description
+      charPosition = script.IndexOf("a portrait");
+      string sceneDescription = script.Substring(charPosition);
+
+      // Make camera directions uppercase
+      cameraDirections = cameraDirections.ToUpper();
+
+      // Make scene description lowercase
+      sceneDescription = sceneDescription.ToLower();
+
+      // Print results
+      Console.WriteLine(cameraDirections);
+      Console.WriteLine(sceneDescription);
     }
   }
 }
