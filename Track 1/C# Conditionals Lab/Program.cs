041f81ac794d3a9bf08c2f80e44738f8f15ea9ae
JanTangonan <janthegreatxc@gmail.com>
2024-08-17 23:29:34 +0800

    C# Conditional Labs 2nd commit

--- Track 1/C# Conditionals Lab/Program.cs
+++ Track 1/C# Conditionals Lab/Program.cs
@@ -17,11 +17,39 @@ namespace ChooseYourOwnAdventure
 
       Console.Write("Type YES or NO: ");
       string noiseChoice = Console.ReadLine();
-
       noiseChoice.ToUpper();
-      
-    
 
+      if (noiseChoice == "NO") {
+        Console.WriteLine("Not much of an adventure if we don't leave our room!");
+        Console.WriteLine("THE END.");
+      }
+      else if (noiseChoice == "YES") {
+        Console.WriteLine("You walk into the hallway and see a light coming from under a door down the hall.");
+        Console.WriteLine("You walk towards it. Do you open it or knock?");
+      }
+
+      Console.WriteLine("Type OPEN or KNOCK");
+      string doorChoice = Console.ReadLine();
+      doorChoice.ToUpper();
+
+      if (doorChoice == "KNOCK") {
+        Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle: \"");
+        Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle: \"");
+
+        Console.WriteLine("Type your answer: ");
+        string riddleAnswer = Console.ReadLine();
+        riddleAnswer.ToUpper();
+        if (riddleAnswer == "NOTHING"){
+          Console.WriteLine("The door opens and NOTHING is there. You turn off the light and run back to your room and lock the door.")
+          Console.WriteLine("THE END.")
+        }
+        else {
+          
+        }
+      }
+      else if (doorChoice == "OPEN") {
+
+      }
     }
   }
 }
