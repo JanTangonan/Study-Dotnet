b24af2c40b05a22a324ee2a8309c0b5858b8105f
JanTangonan <janthegreatxc@gmail.com>
2024-09-18 23:42:36 +0800

    Loops addition

--- Track-1/C#-Galactic-Travel-Agency/obj/Debug/net8.0/C#-Galactic-Travel-Agency.AssemblyInfo.cs
+++ Track-1/C#-Galactic-Travel-Agency/obj/Debug/net8.0/C#-Galactic-Travel-Agency.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#-Galactic-Travel-Agency")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa44f154e37be9c2bc7257b3c9078cb72db86224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc97989186475d08525d1ca73e1d3bb9d63335e")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#-Galactic-Travel-Agency")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#-Galactic-Travel-Agency")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Track-1/C#-Loops/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace WhileLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int emails = 20;
+      Console.WriteLine(emails);
+      //WHILE LOOP
+      
+      while (emails != 0) {
+        emails--;
+        Console.WriteLine($"Remaining Emails: {emails}.");
+      }
+      Console.WriteLine("INBOX ZERO ACHIEVED!");
+
+      string[] websites = { "twitter", "facebook", "gmail" };
+      int c = 0;
+      
+      while (c < websites.Length)
+      { 
+        Console.WriteLine(websites[c]); 
+        c++; 
+      }
+
+      //DO WHILE LOOP
+      int counter = 0;
+      
+      do {
+        Console.WriteLine("Alarm Ringing");
+        counter++;
+      } while (counter < 10);
+      Console.WriteLine("Time for a five minute break.");
+      
+      //FOR LOOP
+      for (int i = 1; i <= 16; i++){
+        CreateTemplate(i);
+      }
+
+      //FOREACH
+      string[] todo = {"respond to email", "make wireframe", "program feature", "fix bugs"};
+      
+      foreach (string list in todo)
+      {
+      Console.WriteLine($"[] {list}");
+      }
+
+      foreach (string sites in websites){
+        Console.WriteLine(sites);
+      }
+    }
+    static void CreateTemplate(int week)
+    {
+      Console.WriteLine($"Week {week}");
+      Console.WriteLine("Announcements: \n \n \n ");
+      Console.WriteLine("Report Backs: \n \n \n");
+      Console.WriteLine("Discussion Items: \n \n \n");
+    }
+  }
+}
