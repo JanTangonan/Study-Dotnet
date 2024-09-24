45d4387a897ac56c22af361f1ef0cfaefe94960b
JanTangonan <janthegreatxc@gmail.com>
2024-09-24 23:24:16 +0800

    abstraction aditions

---
+++ Track-1/C#-Architect-Arithmetic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArchitectArithmetic
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    { 
+      Console.WriteLine($"Total Cost is {Math.Round(CalculateTotalCost(Rectangle(2500, 1500),Circle(375),Triangle(750, 500)),2)} Mexican Pesos");
+    }
+
+    static double Rectangle(double L, double W) {
+      double area = L * W;
+      return area;
+    }
+
+    static double Circle(double R) {
+      double area = (R * R) * Math.PI;
+      return area; 
+    }
+
+    static double Triangle(double B, double H) {
+      double area = 0.5 * B * H;
+      return area;
+    }
+
+    static double CalculateTotalCost(double A, double B, double C) {
+      double totalCost = (A + B + C) * 180;
+      return totalCost;
+    }
+  }
+}
